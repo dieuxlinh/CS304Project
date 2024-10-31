aaac54c4cfd0d8b00bb2469be6715d715b6e5b06
Sophie <sophiefabfive@gmail.com>
2024-10-30 23:47:42 -0400

    Added sample data

---
+++ sample_data.sql
@@ -0,0 +1,12 @@
+-- This is sample data that can be inserted into the database tables.
+
+/* INSERT INTO users (username, email, password_hash, profile_pic) VALUES ("Sophie", "sophie@gmail.com", "123", "456");
+INSERT INTO users (username, email, password_hash, profile_pic) VALUES ("Linh", "Linh@gmail.com", "321", "465");
+INSERT INTO users (username, email, password_hash, profile_pic) VALUES ("Ariel", "Ariel@gmail.com", "132", "654");
+INSERT INTO media (title, media_type, artist) VALUES ("Stormy Weather", 'Song', "Etta James");
+INSERT INTO media (title, media_type, director) VALUES ("The Nightmare Before Christmas", 'Movie', "Henry Selick");
+INSERT INTO reviews (media_id, user_id, review_text, rating) VALUES (1, 1, "Good Song", 10);
+INSERT INTO currents (user_id, media_id, progress) VALUES (1, 2, 50);
+INSERT INTO friends (user_id, friend_id) VALUES (1,2);
+INSERT INTO friends (user_id, friend_id) VALUES (2,3);
+INSERT INTO friends (user_id, friend_id) VALUES (1,3); */
