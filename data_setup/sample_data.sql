b40263fac04acef55b6873bc4f908d5138e1d474
Sophie Thorpe <Sthorpe21178>
2024-11-21 18:04:11 -0500

    All draft changes

---
+++ data_setup/sample_data.sql
@@ -0,0 +1,27 @@
+-- This is sample data that can be inserted into the database tables.
+/* use recap_db;
+
+INSERT INTO users (username, email, password_hash, profile_pic) VALUES 
+("Sophie", "sophie@gmail.com", "123", "456");
+INSERT INTO users (username, email, password_hash, profile_pic) VALUES 
+("Linh", "Linh@gmail.com", "321", "465");
+INSERT INTO users (username, email, password_hash, profile_pic) VALUES 
+("Ariel", "Ariel@gmail.com", "132", "654");
+INSERT INTO users (username, email, password_hash, profile_pic) VALUES 
+("FakeUser", "fake@gmail.com", 111, 111);
+INSERT INTO media (title, media_type, artist) VALUES 
+("Stormy Weather", 'Song', "Etta James");
+INSERT INTO media (title, media_type, director) VALUES 
+("The Nightmare Before Christmas", 'Movie', "Henry Selick");
+INSERT INTO media (title, media_type, author) VALUES 
+("Don Quixote", 'Book', "Miguel de Cervantes");
+INSERT INTO reviews (media_id, user_id, review_text, rating) 
+VALUES (1, 1, "Good Song", 10);
+INSERT INTO currents (user_id, media_id, progress) VALUES (1, 2, 50);
+INSERT INTO currents (user_id, media_id, progress) VALUES (2, 3, 50);
+INSERT INTO friends (user_id, friend_id) VALUES (1,2);
+INSERT INTO friends (user_id, friend_id) VALUES (2,3);
+INSERT INTO friends (user_id, friend_id) VALUES (1,3); */
+/* INSERT INTO friends (user_id, friend_id) VALUES (5, 1);
+INSERT INTO reviews (media_id, user_id, review_text, rating) VALUES 
+(2, 5, "Awesome Movie", 10); */
